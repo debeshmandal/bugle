8d08c6bbc38656cff2b5103a98127bcf624d5286
Debesh Mandal <debesh@debeshmandal.co.uk>
2024-02-17 17:55:55 +0000

    Add setting of key email components

--- main.go
+++ main.go
@@ -11,9 +11,58 @@ func PartToString(p *mail.Part) string {
 	return string(body)
 }
 
-func CreateMessage() *mail.Msg {
+type Config struct {
+	sender    string
+	recipient string
+	subject   string
+	body      string
+}
+
+func GetDefaultConfig() Config {
+	return Config{
+		sender:    "test@example.com",
+		recipient: "test@example.com",
+		subject:   "Test",
+		body:      "Hello World!",
+	}
+}
+
+type ConfigLambda func(*Config)
+
+func WithSubject(s string) ConfigLambda {
+	return func(c *Config) {
+		c.subject = s
+	}
+}
+
+func WithSender(s string) ConfigLambda {
+	return func(c *Config) {
+		c.sender = s
+	}
+}
+
+func WithRecipient(s string) ConfigLambda {
+	return func(c *Config) {
+		c.recipient = s
+	}
+}
+
+func WithBody(s string) ConfigLambda {
+	return func(c *Config) {
+		c.body = s
+	}
+}
+
+func CreateMessage(cls ...ConfigLambda) *mail.Msg {
 	message := mail.NewMsg()
-	message.SetBodyString(mail.TypeTextPlain, "Hello World!")
+	config := GetDefaultConfig()
+	for _, lambda := range cls {
+		lambda(&config)
+	}
+	message.SetBodyString(mail.TypeTextPlain, config.body)
+	message.Subject(config.subject)
+	message.From(config.sender)
+	message.To(config.recipient)
 	return message
 }
 
--- main_test.go
+++ main_test.go
@@ -13,3 +13,17 @@ func TestCreateMessage(t *testing.T) {
 		t.Fatalf(`CreateMessage() = %q; want match for %#q`, body, test_body)
 	}
 }
+
+func TestWithLambda(t *testing.T) {
+	test_config := GetDefaultConfig()
+	msg := CreateMessage(
+		WithBody(test_config.body),
+		WithSender(test_config.body),
+		WithSubject(test_config.body),
+		WithRecipient(test_config.body),
+	)
+	body := PartToString(msg.GetParts()[0])
+	if !regexp.MustCompile(test_config.body).MatchString(body) {
+		t.Fatalf(`CreateMessage() = %q; want match for %#q`, body, test_config.body)
+	}
+}
